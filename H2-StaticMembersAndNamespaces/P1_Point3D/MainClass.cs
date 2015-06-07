46dc4809b9d9b4bafa6b1b4e4a516e068014718a
Svetlozar Kirkov <svetlozark@gmail.com>
2015-06-07 20:00:58 +0300

    update h2

---
+++ H2-StaticMembersAndNamespaces/P1_Point3D/MainClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P1_Point3D
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            Point3D initialPoint = new Point3D();
+            Point3D constructedPoint = new Point3D(2,4,5);
+
+            initialPoint.X = 4.5;   //changing individual points in the instances
+            constructedPoint.Z = 12;
+
+            Console.WriteLine(initialPoint);    // printing the objects to the console
+            Console.WriteLine(constructedPoint);
+        }
+    }
+}
---
+++ H2-StaticMembersAndNamespaces/P1_Point3D/Point3D.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace P1_Point3D
+{
+    class Point3D
+    {
+        private double x, y, z = 0d;
+        private static readonly Point3D StartingPoint = new Point3D(0, 0, 0);
+
+        public double X
+        {
+            get { return this.x; }
+            set { this.x = value; }
+        }
+        public double Y
+        {
+            get { return this.y; }
+            set { this.y = value; }
+        }
+        public double Z
+        {
+            get { return this.z; }
+            set { this.z = value; }
+        }
+
+        public Point3D()    //empty constructor, equal to StartingPoint
+        {
+            
+        }
+
+        public Point3D(double x, double y, double z)    //constructor with all three points as parameters
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[ x={0}, y={1}, z={2} ]", this.x, this.y, this.z);
+        }
+    }
+}
