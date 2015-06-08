fe80810298d47ea3d20ca0102bfc1abf26e4fe96
Svetlozar Kirkov <svetlozark@gmail.com>
2015-06-09 00:28:59 +0300

    initial H3

---
+++ H3-ExceptionHandling/P1_SquareRoot/P1_SquareRoot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P1_SquareRoot
+{
+    class P1_SquareRoot
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
---
+++ H3-ExceptionHandling/P2_EnterNumbers/P2_EnterNumbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P2_EnterNumbers
+{
+    class P2_EnterNumbers
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
