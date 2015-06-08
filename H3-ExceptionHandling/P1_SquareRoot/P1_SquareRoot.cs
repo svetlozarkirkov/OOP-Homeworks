9694104f555ec33807bc905083fda9bebe2bd82d
Svetlozar Kirkov <svetlozark@gmail.com>
2015-06-09 00:42:21 +0300

    updated h3-1

--- H3-ExceptionHandling/P1_SquareRoot/P1_SquareRoot.cs
+++ H3-ExceptionHandling/P1_SquareRoot/P1_SquareRoot.cs
@@ -6,7 +6,32 @@ namespace P1_SquareRoot
     {
         static void Main()
         {
-
+            Console.Write("This program calculates Square Root!\n" +
+                          "Enter number: ");
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num < 0)
+                {
+                    Console.WriteLine("Invalid number");
+                }
+                else
+                {
+                    Console.WriteLine("Square root: {0}", Math.Sqrt(num));
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Invalid number!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("This is not a number!?");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye :P");
+            }
         }
     }
 }
