27f7c7dc0643ebd97c7e8b5698a5336cc49857fc
Svetlozar Kirkov <svetlozark@gmail.com>
2015-06-25 11:43:36 +0300

    finished h8-3

---
+++ H8-CommonTypeSystem/P3_StringDisperser/StringDisperser.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P3_StringDisperser
+{
+    class StringDisperser : IComparable<StringDisperser>, ICloneable, IEnumerator, IEnumerable
+    {
+        public string[] StringElements { get; set; }
+        public List<char> CharElements { get; set; }
+        private int position = -1;
+
+        public int CharSum
+        {
+            get
+            {
+                return this.CharElements.Sum(ch => (int) ch);
+            }
+        }
+
+        public StringDisperser(params string[] args)
+        {
+            this.StringElements = args;
+            this.CharElements = new List<char>();
+            if (this.StringElements.Length == 0)
+            {
+                return;
+            }
+            foreach (var stringElement in this.StringElements)
+            {
+                foreach (var charElement in stringElement)
+                {
+                    this.CharElements.Add(charElement);
+                }
+            }
+        }
+
+        protected bool Equals(StringDisperser other)
+        {
+            return Equals(CharElements, other.CharElements);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((StringDisperser) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (CharElements != null ? CharElements.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(StringDisperser left, StringDisperser right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(StringDisperser left, StringDisperser right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override string ToString()
+        {
+            string result = string.Format("Strings: [ {0} ]\nChars: [ {1} ]\nChar Sum: {2}",
+                string.Join(", ", this.StringElements), string.Join(" ", this.CharElements),this.CharSum);
+            return result;
+        }
+
+        public StringDisperser Clone()
+        {
+            return new StringDisperser(this.StringElements);
+        }
+
+        object ICloneable.Clone() { return Clone(); }
+
+        public int CompareTo(StringDisperser other)
+        {
+            return this.CharSum - other.CharSum;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return (IEnumerator)this;
+        }
+        public bool MoveNext()
+        {
+            position++;
+            return (position < this.CharElements.Count);
+        }
+        public void Reset()
+        { position = 0; }
+        public object Current
+        {
+            get { return this.CharElements[position]; }
+        }
+    }
+}
---
+++ H8-CommonTypeSystem/P3_StringDisperser/TestClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace P3_StringDisperser
+{
+    class TestClass
+    {
+        static void Main()
+        {
+            StringDisperser people = new StringDisperser("Pesho", "Maria", "Gosho");
+            StringDisperser peopleClone = people.Clone();
+            
+            StringDisperser cities = new StringDisperser("Varna","Sofia","Berlin","New York");
+            StringDisperser animals = new StringDisperser("Cat","Dog","Rabbit","Goat");
+            
+            var strDspList = new List<StringDisperser>
+            {
+                animals,
+                cities,
+                people
+            };
+
+            foreach (var strdsp in strDspList)
+            {
+                Console.WriteLine(strdsp);
+                Console.WriteLine(new string('-',50));
+            }
+
+            strDspList.Sort();  // by total char sum
+
+            foreach (var element in strDspList)
+            {
+                foreach (var ch in element)
+                {
+                    Console.Write(ch + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
